529216d581590267ec9aa1f53c6477bc5ba308bf
abbst <abbsthepandahead@gmail.com>
2025-08-29 13:29:12 -0600

    Added more functionality to inventory and drinks

--- Main.java
+++ Main.java
@@ -18,7 +18,18 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int userOption = 0;
         int userDrinkOption = 0;
+        int userInventoryOption = 0;
+        int addMoreIngredients = 0;
+        int howMany = 0;
         boolean isRunning = true;
+
+
+        //Ingredients
+        int hotWater = 20;
+        int cocoPowder = 5;
+        int coffeeGrinds = 10;
+        int espressoShots = 5;
+        int greenTeaBags = 5;
 
         System.out.println("""
         ************************************************
@@ -34,7 +45,41 @@ public class Main {
             userOption = scanner.nextInt();
 
             if (userOption == 1) {
-                System.out.println("INVENTORY");
+                System.out.printf("""
+                    INVENTORY
+            
+            Hot Water:           %4d\n
+            Coffee:              %4d\n
+            Hot Chocolate:       %4d\n
+            Espresso:            %4d\n
+            Tea:                 %4d\n
+
+            1] Add More Ingredients  
+            2] Exit 
+                """, hotWater, coffeeGrinds, cocoPowder, espressoShots, greenTeaBags);
+            
+            userInventoryOption = scanner.nextInt();
+            if(userInventoryOption == 1){
+                System.out.println(""" 
+                    What do you want to refill?
+            1] Hot Water
+            2] Coffee
+            3] Hot Chocolate
+            4] Tea        
+                    """);
+            addMoreIngredients = scanner.nextInt();
+            if(addMoreIngredients == 1){
+                System.out.println("How much more Hot Water do you want to add?");
+                howMany = scanner.nextInt();
+                hotWater += howMany;
+                System.out.println(hotWater);
+            }
+            }
+
+
+
+
+
             } else if (userOption == 2) {
                 System.out.println(""" 
                                     Select a drink
@@ -42,20 +87,51 @@ public class Main {
                     """);
                 userDrinkOption = scanner.nextInt();
                 switch (userDrinkOption) {
-                    case 1 ->
-                        System.out.println("You picked coffee. Enjoy!");
-                    case 2 ->
-                        System.out.println("You picked hot chocolate. Enjoy!");
-                    case 3 ->
-                        System.out.println("You picked espresso. Enjoy!");
-                    case 4 ->
-                        System.out.println("You picked tea. Enjoy!");
+                    case 1 -> {
+                        if (coffeeGrinds == 0) {
+                            System.out.println("Sorry, out of Coffee :(");
+                        } else {
+                            System.out.println("You picked Coffee. Enjoy!");
+                            coffeeGrinds-=1;
+                            hotWater-=1;
+
+                        }
+                    }
+                    case 2 -> {
+                        if (cocoPowder == 0) {
+                            System.out.println("Sorry, out of Hot Chocolate :(");
+                        } else {
+                            System.out.println("You picked Hot Chocolate. Enjoy!");
+                            cocoPowder -= 1;
+                             hotWater-=1;
+                        }
+                    }
+                    case 3 -> {
+                        if (espressoShots == 0) {
+                            System.out.println("Sorry, out of Espresso :(");
+                        } else {
+                            System.out.println("You picked Espresso. Enjoy!");
+                            espressoShots -= 1;
+                             hotWater-=1;
+                        }
+                    }
+                    case 4 -> {
+                        if (greenTeaBags == 0) {
+                            System.out.println("Sorry, out of Tea :(");
+                        } else {
+                            System.out.println("You picked Tea. Enjoy!");
+                            greenTeaBags-=1;
+                            hotWater-=1;
+                        }
+                    }
                     default ->
                         System.out.println("Not a valid option!");
                 }
+
             } else if (userOption == 3) {
                 isRunning = false;
                 System.out.println("Thank you! Goodbye! :)");
+
             } else {
                 System.out.println("Not a valid option!");
             }
